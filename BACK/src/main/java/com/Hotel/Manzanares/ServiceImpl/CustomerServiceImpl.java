7fd5b4e468df8f04741c3bf015ae6240905061fe
Jose Alberto Ibáñez Marín <71636425+JoseikoUA@users.noreply.github.com>
2024-06-30 17:13:10 +0200

    Cambios

--- BACK/src/main/java/com/Hotel/Manzanares/ServiceImpl/CustomerServiceImpl.java
+++ BACK/src/main/java/com/Hotel/Manzanares/ServiceImpl/CustomerServiceImpl.java
@@ -68,6 +68,7 @@ public class CustomerServiceImpl implements CustomerService {
                 elemento.setTipo(usuario.getTipo());
                 elemento.setNombre(usuario.getNombre());
                 elemento.setTelefono(usuario.getTelefono());
+                elemento.setFechaNacimiento(usuario.getFechaNacimiento());
 
                 customerRepository.save(elemento);
             }
